127f94971001b95a673bdbbc07e5e6251dd0a816
matias1364 <matiasleoncontreras@gmail.com>
2023-08-15 18:47:16 -0300

    add confg

--- src/main/java/com/security/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/security/demo/security/JwtAuthenticationFilter.java
@@ -23,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private  JwtTokenProvider jwtGenerator;
 
-    public String obtenerToken(HttpServletRequest request){
+    private String obtenerToken(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
         if(StringUtils.hasText(bearerToken)&& bearerToken.startsWith("Bearer ")){
             return  bearerToken.substring(7,bearerToken.length());
